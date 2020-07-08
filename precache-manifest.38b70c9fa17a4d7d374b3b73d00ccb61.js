d44ced07772d9594542feed6c553a361ce06b5e6
sdc02408 <sdc02408@naver.com>
2020-07-08 14:14:35 +0900

    Updates

---
+++ precache-manifest.38b70c9fa17a4d7d374b3b73d00ccb61.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7632af8618743a6358297056bd14aab",
+    "url": "/react_csnetflix/index.html"
+  },
+  {
+    "revision": "3f63e5aa43ad50660d30",
+    "url": "/react_csnetflix/static/css/2.689b7e3c.chunk.css"
+  },
+  {
+    "revision": "463309e90283d2dc95b7",
+    "url": "/react_csnetflix/static/css/main.6479ea27.chunk.css"
+  },
+  {
+    "revision": "3f63e5aa43ad50660d30",
+    "url": "/react_csnetflix/static/js/2.4271eee7.chunk.js"
+  },
+  {
+    "revision": "b3339fb73e1e6b42b25ee43fa12a6f40",
+    "url": "/react_csnetflix/static/js/2.4271eee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "463309e90283d2dc95b7",
+    "url": "/react_csnetflix/static/js/main.4b277df4.chunk.js"
+  },
+  {
+    "revision": "563f4ae5ed8cfaba3dfd",
+    "url": "/react_csnetflix/static/js/runtime-main.fa535d85.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react_csnetflix/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react_csnetflix/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react_csnetflix/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react_csnetflix/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "69c7de72c4c202ee398aeadb66c66687",
+    "url": "/react_csnetflix/static/media/star.69c7de72.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a549af34630b48144695b0613957d353.js"
+  "/react_csnetflix/precache-manifest.38b70c9fa17a4d7d374b3b73d00ccb61.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_csnetflix/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
